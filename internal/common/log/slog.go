75133e3ed9c83aeae0b010d6a0d3111ac1b044d3
quangbach <167604957+quangbach27@users.noreply.github.com>
2026-06-06 22:18:20 +0700

    fix: update logging handler initialization for development environment

--- internal/common/log/slog.go
+++ internal/common/log/slog.go
@@ -11,13 +11,13 @@ func Init(level slog.Level) {
 	handlerOpts := &slog.HandlerOptions{Level: level}
 
 	var handler slog.Handler
-	if os.Getenv("APP_ENV") == "production" {
-		handler = slog.NewJSONHandler(os.Stderr, handlerOpts)
-	} else {
+	if os.Getenv("APP_ENV") == "dev" {
 		handler = humanslog.NewHandler(os.Stderr, &humanslog.Options{
 			HandlerOptions: handlerOpts,
 			TimeFormat:     "[15:04:05.000]",
 		})
+	} else {
+		handler = slog.NewJSONHandler(os.Stderr, handlerOpts)
 	}
 
 	slog.SetDefault(slog.New(handler))
